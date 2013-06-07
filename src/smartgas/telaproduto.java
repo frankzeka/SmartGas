2c43b95b9b32f0b5afbdc3223b2ec71e9e0d3100
info52 <info52@escola1956>
2013-06-07 20:24:31 -0300

    fiz tela pesquisa produto.

--- src/smartgas/telaproduto.java
+++ src/smartgas/telaproduto.java
@@ -41,16 +41,16 @@ private produto fichas;
         jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        entradaquantidade = new javax.swing.JTextField();
+        EntradaTipo = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         entradapreçounitario = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         entradapeso = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        entradatipo = new javax.swing.JTextField();
+        entradaquantidade = new javax.swing.JTextField();
         jPanel3 = new javax.swing.JPanel();
         Salvar = new javax.swing.JButton();
-        Anterior = new javax.swing.JButton();
+        Pesquisar = new javax.swing.JButton();
         Proximo = new javax.swing.JButton();
 
         jButton1.setText("jButton1");
@@ -66,11 +66,14 @@ private produto fichas;
             .addGap(0, 100, Short.MAX_VALUE)
         );
 
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
             }
         });
+        getContentPane().setLayout(null);
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
@@ -87,60 +90,75 @@ private produto fichas;
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(0, 65, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(entradapeso)
+                    .addComponent(EntradaTipo, javax.swing.GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE))
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(entradaquantidade, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel2)
-                        .addGap(4, 4, 4))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(18, 18, 18)
-                        .addComponent(entradapeso)
-                        .addGap(35, 35, 35)
-                        .addComponent(jLabel4)
-                        .addGap(32, 32, 32)))
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(entradapreçounitario, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(47, 47, 47))
+                        .addGap(56, 56, 56)
+                        .addComponent(jLabel2))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(entradatipo, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())))
+                        .addGap(62, 62, 62)
+                        .addComponent(jLabel1)))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(entradapreçounitario, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(entradaquantidade, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(91, 91, 91))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(entradaquantidade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2)
-                    .addComponent(entradapreçounitario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(24, 24, 24)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(entradapeso, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4)
-                    .addComponent(entradatipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(47, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(EntradaTipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)
+                            .addComponent(entradapreçounitario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel4)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(jLabel1)
+                                .addComponent(entradaquantidade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(entradapeso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(53, Short.MAX_VALUE))
         );
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 513, 147);
 
         jPanel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
         Salvar.setText("Salvar");
+        Salvar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                SalvarMouseClicked(evt);
+            }
+        });
         Salvar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 SalvarActionPerformed(evt);
             }
         });
 
-        Anterior.setText("Pesquisar");
-        Anterior.addMouseListener(new java.awt.event.MouseAdapter() {
+        Pesquisar.setText("Pesquisar");
+        Pesquisar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                AnteriorMouseClicked(evt);
+                PesquisarMouseClicked(evt);
             }
         });
 
@@ -159,46 +177,33 @@ private produto fichas;
                 .addGap(46, 46, 46)
                 .addComponent(Salvar)
                 .addGap(43, 43, 43)
-                .addComponent(Anterior)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 197, Short.MAX_VALUE)
+                .addComponent(Pesquisar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 201, Short.MAX_VALUE)
                 .addComponent(Proximo)
                 .addGap(56, 56, 56))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(21, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(Salvar)
-                    .addComponent(Anterior)
+                    .addComponent(Pesquisar)
                     .addComponent(Proximo))
-                .addGap(62, 62, 62))
+                .addGap(59, 59, 59))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(34, 34, 34))
-        );
+        getContentPane().add(jPanel3);
+        jPanel3.setBounds(0, 153, 513, 59);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void SalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalvarActionPerformed
-        // TODO add your handling code here:
+         // TODO add your handling code here:
          ficha = new produto();
-    ficha.quantidade=Integer.parseInt(entradaquantidade.getText());
-    entradaquantidade.setText("");
+    ficha.quantidade=Integer.parseInt(EntradaTipo.getText());
+    EntradaTipo.setText("");
     
     ficha.precounitario=Integer.parseInt(entradapreçounitario.getText());
     entradapreçounitario.setText("");
@@ -206,8 +211,8 @@ private produto fichas;
     ficha.peso=Integer.parseInt(entradapeso.getText());
     entradapeso.setText("");
     
-    ficha.tipo = entradatipo.getText();
-    entradatipo.setText("");
+    ficha.tipo = entradaquantidade.getText();
+    entradaquantidade.setText("");
     
     total++;
         atual = atual+1; //vai para a prox ficha
@@ -245,32 +250,16 @@ private produto fichas;
             atual=atual++;
    
         }
-        entradaquantidade.setText(Integer.toString(ficha.quantidade));
+        EntradaTipo.setText(Integer.toString(ficha.quantidade));
         entradapreçounitario.setText(Float.toString(ficha.precounitario));
         entradapeso.setText(Integer.toString(ficha.peso));
-        entradatipo.setText(ficha.tipo);  
+        entradaquantidade.setText(ficha.tipo);  
     }//GEN-LAST:event_ProximoActionPerformed
 
-    private void AnteriorMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_AnteriorMouseClicked
-        // TODO add your handling code here:
-        if (atual<=0){
-            //avisa o usuario que chegou no inicio
-            
-            atual=0;//volta para 0 para nao dar problema
-        }else{
-            atual = atual-1;//volta para a anterior 
-            
-        }
-        ficha = new produto();
-        ficha = fichario.get(atual);// pega a ficha atual do fichario
-        entradaquantidade.setText(Integer.toString(ficha.quantidade));
-        entradapreçounitario.setText(Float.toString(ficha.precounitario));
-        entradapeso.setText(Integer.toString(ficha.peso));
-        entradatipo.setText(ficha.tipo);      
-      
-          
-    
-    }//GEN-LAST:event_AnteriorMouseClicked
+    private void PesquisarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PesquisarMouseClicked
+        // TODO add your handling code here:    
+        new Pesquisaproduto().setVisible(true);
+    }//GEN-LAST:event_PesquisarMouseClicked
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         // TODO add your handling code here:
@@ -297,14 +286,49 @@ private produto fichas;
         atual = total-1;
         ficha = new produto();
         ficha = fichario.get(atual);// pega a ficha atual do fichario
-        entradaquantidade.setText(Integer.toString(ficha.quantidade));
+        EntradaTipo.setText(Integer.toString(ficha.quantidade));
         entradapreçounitario.setText(Float.toString(ficha.precounitario));
         entradapeso.setText(Integer.toString(ficha.peso));
-        entradatipo.setText(ficha.tipo);     
+        entradaquantidade.setText(ficha.tipo);     
                 
         }
     
     }//GEN-LAST:event_formWindowOpened
+
+    private void SalvarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SalvarMouseClicked
+        // TODO add your handling code here:
+        ficha  = new produto();       
+        ficha.tipo = EntradaTipo.getText();
+        ficha.peso = Integer.parseInt(entradapeso.getText());
+        ficha.precounitario = Integer.parseInt(entradapreçounitario.getText());
+        ficha.quantidade = Integer.parseInt(entradaquantidade.getText());
+        //acabou a coleta de dados
+        //Limpa os campos
+        EntradaTipo.setText("");
+        entradapeso.setText("");
+        entradapreçounitario.setText("");
+        entradaquantidade.setText("");
+        //limpando os campos
+        
+        //atualiza o contador
+        atual++;// faz a mesma coisa que atual=atual+1
+        //mostra em qual registro está(registro atual)
+
+        fichario.add(ficha);
+        FileOutputStream filestream;
+        try {
+            filestream = new FileOutputStream("fornecedor.ser");
+            try {
+                ObjectOutputStream arquivo = new ObjectOutputStream(filestream);
+                arquivo.writeObject(fichario);
+                arquivo.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Telafornecedor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Telafornecedor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_SalvarMouseClicked
 
     /**
      * @param args the command line arguments
@@ -348,13 +372,13 @@ private produto fichas;
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Anterior;
+    private javax.swing.JTextField EntradaTipo;
+    private javax.swing.JButton Pesquisar;
     private javax.swing.JButton Proximo;
     private javax.swing.JButton Salvar;
     private javax.swing.JTextField entradapeso;
     private javax.swing.JTextField entradapreçounitario;
     private javax.swing.JTextField entradaquantidade;
-    private javax.swing.JTextField entradatipo;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
